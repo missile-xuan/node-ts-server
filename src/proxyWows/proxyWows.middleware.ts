5690565232f2d91ed5fb331decef846f4b4e4abf
missile-xuan <missile_xuan@gmail.com>
2024-11-29 15:07:12 +0800

    调整跨域代理

--- src/proxyWows/proxyWows.middleware.ts
+++ src/proxyWows/proxyWows.middleware.ts
@@ -5,7 +5,7 @@ import { Request, Response, NextFunction } from 'express';
 export class SetHeaderMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     res.setHeader('Cache-Control', 'no-store');
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.setHeader('Access-Control-Allow-Headers', req.headers['access-control-request-headers']||'Content-Type');
 
